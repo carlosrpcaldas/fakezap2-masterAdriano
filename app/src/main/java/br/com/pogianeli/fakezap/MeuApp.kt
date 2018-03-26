9254130bf8735eca359e52058f1ba125d734b092
carlos.caldas <carlos.caldas@totvs.com.br>
2018-03-26 17:00:29 -0300

    Initial commit

---
+++ app/src/main/java/br/com/pogianeli/fakezap/MeuApp.kt
@@ -0,0 +1,23 @@
+package br.com.pogianeli.fakezap
+
+import android.app.Application
+import com.facebook.stetho.Stetho
+import com.orhanobut.hawk.Hawk
+import com.orhanobut.hawk.NoEncryption
+
+/**
+ * Created by logonrm on 19/03/2018.
+ */
+
+class MeuApp : Application (){
+
+    override fun onCreate() {
+        super.onCreate()
+        Stetho.initializeWithDefaults(this);
+        Hawk.init(this)
+                //apenas para teste
+                //.setEncryption(NoEncryption())
+                .build();
+    }
+
+}
---
+++ app/src/main/java/br/com/pogianeli/fakezap/firebase/fcm/MeuFirebaseInstanceIdService.kt
@@ -0,0 +1,18 @@
+package br.com.pogianeli.fakezap.firebase.fcm
+
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.iid.FirebaseInstanceIdService
+import com.orhanobut.hawk.Hawk
+
+/**
+ * Created by logonrm on 19/03/2018.
+ */
+
+class MeuFirebaseInstanceIdService : FirebaseInstanceIdService (){
+
+    override fun onTokenRefresh() {
+        super.onTokenRefresh()
+        Hawk.put("TOKENFIREBASE",FirebaseInstanceId.getInstance().token)
+    }
+
+}
